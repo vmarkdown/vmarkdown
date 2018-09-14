733c2baaf71b4fcc7e456c38bf879255c768c4b0
yucopowo <yucopowo@gmail.com>
2018-09-14 22:43:20 +0800

    add react example

--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
 import vremark from 'vremark';
 
 export default function vmarkdown(md, options) {
-    // var file = vremark()
-    //     .use(vremark.toc, {
-    //         // heading: 'TOO'
-    //     })
-    //     // .use(vremark.vdom, options)
-    //     .use(vremark.html)
-    //     .processSync(md);
-
-    var parser = vremark();
-
-    parser = parser.use(vremark.math);
-    parser = parser.use(vremark.katex);
-
-    if(options.toc) {
-        parser = parser.use(vremark.toc, options.toc);
-    }
-    if(options.vdom) {
-        parser = parser.use(vremark.vdom, options.vdom);
-    }
-    if(options.html) {
-        parser = parser.use(vremark.html, options.html);
-    }
-
-
-
-    var file = parser.processSync(md);
-
-    return file.contents;
+    return vremark(md, options);
 }
--- src/libs/vremark.js
+++ src/libs/vremark.js
@@ -1,18 +1,32 @@
 import remark from 'remark';
-
 import vdom from './vdom';
-remark.vdom = vdom;
-
 import toc from 'remark-toc';
-remark.toc = toc;
-
 import html from 'remark-html';
-remark.html = html;
-
 import math from 'remark-math';
-remark.math = math;
-
 import katex from 'remark-html-katex';
-remark.katex = katex;
 
-export default remark;
+export default function vremark(md, options) {
+
+    let processor = remark();
+
+    if(options.math) {
+        processor = processor.use(math, options.math);
+    }
+    if(options.katex) {
+        processor = processor.use(katex, options.katex);
+    }
+    if(options.toc) {
+        processor = processor.use(toc, options.toc);
+    }
+    if(options.vdom) {
+        processor = processor.use(vdom, options.vdom);
+    }
+    if(options.html) {
+        processor = processor.use(html, options.html);
+    }
+
+    const vfile = processor.processSync(md);
+
+    return vfile.contents;
+
+}
