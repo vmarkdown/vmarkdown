b3a9a1baddbb3031c342117b605f676e315b81e0
yucopowo <yucopowo@gmail.com>
2018-09-15 08:10:48 +0800

    add react example

--- test.js
+++ test.js
@@ -1,22 +1,31 @@
 const remark = require('remark')
-const math = require('remark-math')
-const katex = require('remark-html-katex') // Use remark-html-katex
-const html = require('remark-html')
+// const math = require('remark-math')
+// const katex = require('remark-html-katex') // Use remark-html-katex
+// const html = require('remark-html')
+var remark2rehype = require('remark-rehype');
+var html = require('rehype-stringify')
 
 // Raw String => MDAST => transformed MDAST => HTML
-const processor = remark()
-    .use(math)
-    .use(katex)
-    .use(html)
+const processor = remark().use(remark2rehype).use(html)
+    // .use(math)
+    // .use(katex)
+    // .use(html)
 
 
-const rawString = `
-$$
-L = \\frac{1}{2} \\rho v^2 S C_L
-$$
-`
+// const rawString = `
+// $$
+// L = \\frac{1}{2} \\rho v^2 S C_L
+// $$
+// `
 
-const result = processor.processSync(rawString).toString();
+const md = `# Hello world
+
+> Block quote.
+
+Some _emphasis_, **importance**, and \`code\`.`;
+
+
+const result = processor.processSync(md).toString();
 
 console.log(result);
 
